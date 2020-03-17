0349fc3081ead96263257877e4f02d98b6f12478
tjddls1728 <61854982+tjddls1728@users.noreply.github.com>
2020-03-17 17:02:07 +0900

    완성

--- app/src/main/java/kr/co/a20200317_04_androidfunction01/SecondActivity.java
+++ app/src/main/java/kr/co/a20200317_04_androidfunction01/SecondActivity.java
@@ -1,6 +1,7 @@
 package kr.co.a20200317_04_androidfunction01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -10,16 +11,19 @@ import kr.co.a20200317_04_androidfunction01.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
+    ActivitySecondBinding binding = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_second);
 
     String sandmessage = getIntent().getStringExtra("message");
 
         Log.d("받아온String",sandmessage);
+
+        binding.messageTxt.setText(sandmessage);
 
     }
 }
