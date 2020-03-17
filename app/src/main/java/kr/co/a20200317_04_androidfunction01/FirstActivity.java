67df98c60a7aef0ce1e5d3ad53206f7beda2aeea
tjddls1728 <61854982+tjddls1728@users.noreply.github.com>
2020-03-17 16:08:16 +0900

    첫번째 액티비티 추가

---
+++ app/src/main/java/kr/co/a20200317_04_androidfunction01/FirstActivity.java
@@ -0,0 +1,19 @@
+package kr.co.a20200317_04_androidfunction01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.a20200317_04_androidfunction01.databinding.ActivityFirstBinding;
+
+public class FirstActivity extends AppCompatActivity {
+
+    ActivityFirstBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_first);
+    }
+}
